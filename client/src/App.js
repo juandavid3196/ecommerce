8deb1c05a5b8277ba2ae3c786c7cbd330d97ea9c
juan david rios <juandavid3196@gmail.com>
2023-03-16 20:25:30 -0500

    nav structure,foldelrs,material-icon,layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,53 @@
-
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+import Home from './pages/Home/Home';
+import Products from './pages/Products/Products';
+import Product from './pages/Product/Product';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
+const Layaout = () => {  /* the Layaout content de components that always stay in every page
+                            Outlet Have another routes like children */ 
+  return (
+    <div className='app'> 
+      <Navbar/>
+      <Outlet/>  
+      <Footer/>
+    </div>
+  )
+}
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layaout/>,
+    children : [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "/products/:id",
+        element: <Products/>,
+      },
+      {
+        path: "/product/:id",
+        element: <Product/>,
+      },
+    ]
+  },
+ 
+])
 
 function App() {
   return (
     <>
-    Home
+    <RouterProvider router={router} />
     </>
   );
 }
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className="wrapper">
+            <div className="left">
+                <div className="item">
+                    <img src="/img/en.png" alt="" />
+                    <KeyboardArrowDownIcon/>
+                </div>
+                <div className="item">
+                    <span>USD</span>
+                    <KeyboardArrowDownIcon/>
+                </div>
+                <div className="item">
+                    <Link to="/products/1">Women</Link>
+                </div>
+                <div className="item">
+                    <Link to="/products/2">Men</Link>
+                </div>
+                <div className="item">
+                    <Link to="/products/3">Children</Link>
+                </div>
+            </div>
+            <div className="center">
+                <Link to="/">TESTSTORE</Link>
+            </div>
+            <div className="right">
+                <div className="center">
+                    <Link to="/">Homepage</Link>
+                </div>
+                <div className="center">
+                    <Link to="/">About</Link>
+                </div>
+                <div className="center">
+                    <Link to="/">Contact</Link>
+                </div>
+                <div className="center">
+                    <Link to="/">Store</Link>
+                </div>
+                <div className="icons">
+                    <SearchIcon/>
+                    <PersonOutlineOutlinedIcon/>
+                    <FavoriteBorderOutlinedIcon/>
+                    <div className="cartIcon">
+                        <ShoppingCartOutlinedIcon/>
+                        <span>0</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
