d5a700bd8c3f9ef0f0db1af673e688fe73364497
juan david rios <juandavid3196@gmail.com>
2023-03-23 17:15:23 -0500

    get data from strapi, .env

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,43 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './FeaturedProducts.scss';
 import Card from '../Card/Card';
+import axios from "axios";
+
 
 const FeaturedProducts = ({type}) => {
 
-    const data = [
-        {
-            id:1,
-            img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-            img2: 'https://images.pexels.com/photos/10669636/pexels-photo-10669636.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-            title: 'long Sleeve Graphic T-shirt',
-            isNew: true,
-            oldPrice: 19,
-            price: 12 
-        },
-        {
-            id:2,
-            img: 'https://images.pexels.com/photos/1206873/pexels-photo-1206873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-            title: 'Coat',
-            isNew: true,
-            oldPrice: 19,
-            price: 12 
-        },
-        {
-            id:3,
-            img: 'https://images.pexels.com/photos/1007018/pexels-photo-1007018.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-            title: 'Skirt',
-            oldPrice: 19,
-            price: 12 
-        },
-        {
-            id:4,
-            img: 'https://images.pexels.com/photos/1261422/pexels-photo-1261422.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-            title: 'Hat',
-            oldPrice: 19,
-            price: 12 
-        }
-    ]
+    const REACT_APP_API_TOKEN = "f80b088fd04d03978806e3077d54732762e22a399c2a6f8111a4c41d33a05ee0df29b51b26116a88dc61e93e16f1fd1d2adf0030405910ee58fc1bbd65c12aa020566dbf7e4a7930c77ea3bdd66205af68acd489179920787367f1c7221e58a8068c4ae927c799150704238f85fbc026eff891f08a27f1a51acecc8916f8bee7";
+    const REACT_APP_API_URL = "http://localhost:1337/api";
 
+    const [data,setData] = useState([]);
+
+    useEffect(()=> {
+        const fetchData = async ()=> {
+            try {
+                const res = await axios.get(REACT_APP_API_URL+ "/products?populate=*", {
+                    headers :{
+                        Authorization: "bearer " + REACT_APP_API_TOKEN,
+                    },
+                })
+               setData(res.data.data);
+               console.log(res.data.data)
+            } catch (error) {
+                console.log(error)
+            }
+        };
+        fetchData();
+    },[]);
 
   return (
     <div className='featuredProducts'>
